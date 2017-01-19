3cae44e6589b7c31c0bec2d2bcad669ce62a99b6
Sahana Vishwanath <1tbhd@D-69-91-211-217.dhcp4.washington.edu>
2017-01-19 15:09:47 -0800

    Update on exercises 2, 3, and 6

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,41 +1,45 @@
 # Load R's "USPersonalExpenditure" dataset using the "data()" function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data("USPersonalExpenditure")
 
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can simply pass the USPersonalExpenditure variable as an argument
 # to the `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+us.personal.exp <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
-
+colnames(us.personal.exp)
 
 # Why are they so strange? Think about whether you could use a number like 1940
 # with dollar notation!
 
 # What are the row names of your dataframe?
-
+rownames(us.personal.exp)
 
 # Create a column "category" that is equal to your rownames
-
+us.personal.exp$category <- rownames(us.personal.exp)
 
 # How much money was spent on personal care in 1940?
-
+pers.care.1940 <- us.personal.exp["Personal Care", "X1940"]
 
 # How much money was spent on Food and Tobacco in 1960?
-
+food.tobacoo.1960 <- us.personal.exp["Food and Tobacco", "X1960"]
 
 # What was the highest expenditure category in 1960?
 # Hint: use the `max()` function to find the largest, then compare that to the column
-
+highest.exp.1960 <- us.personal.exp$category[us.personal.exp$X1960 == max(us.personal.exp$X1960)]
 
 # Define a function `DetectHighest` that takes in a year as a parameter, and
 # returns the highest spending category of that year
-
+DetectHighest <- function(year){
+  col <- paste0("X", year)
+  return(us.personal.exp$category[us.personal.exp[,col] == max(us.personal.exp[,col])])
+}
 
 # Using your function, determine the highest spending category of each year
+highest.spend.1950 <- DetectHighest(1950)
